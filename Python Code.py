d7ed275a0b894f11979149f14b387bc48da20943
Riddhi'sChaoticWorld <130882317+Riddhiman2005@users.noreply.github.com>
2023-06-04 00:44:02 +0530

    Update Python Code.py

--- Python Code.py
+++ Python Code.py
@@ -3,23 +3,27 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 
-# Set up the figure and axis
+# Setting up the figure and axis
+
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal', xlim=(-1.2, 1.2), ylim=(-1.2, 1.2))
 ax.set_axis_off()
 
-# Create the sun
+# Creation of the sun
+
 sun = plt.Circle((0, 0), 0.1, color='yellow')
 ax.add_artist(sun)
 
-# Create the Earth's orbit
+# Let's create the Earth's orbit
 orbit = plt.Circle((0, 0), 1, color='blue', fill=False)
 ax.add_artist(orbit)
 
-# Create the Earth
+# Creation of the Earth
+
 earth, = ax.plot([], [], 'o', color='green')
 
-# Set up animation parameters
+# Setting up animation parameters
+
 frames = 360  # Number of frames
 angle = np.linspace(0, 2 * np.pi, frames)  # Angle for each frame
 x = np.cos(angle)  # x-coordinates of Earth's position
@@ -31,7 +35,9 @@ def update(frame):
     return earth,
 
 # Create the animation
+
 ani = FuncAnimation(fig, update, frames=frames, interval=50, blit=True)
 
 # Show the animation
+
 plt.show()
